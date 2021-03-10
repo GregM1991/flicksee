a0cd249ad38e9a82f5a63b1cf070ce0369990337
Greg Martin <gregm31@live.com>
2021-03-10 20:03:43 +1100

    Finally sorted out the env vars, my god

--- src/components/MainPage/index.jsx
+++ src/components/MainPage/index.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import GridWrapper from "components/GridWrapper";
 import SearchBar from "components/SearchBar";
+import axios from "axios";
 
 const CustomGridWrapper = styled(GridWrapper)`
   padding-top: 80px;
 `;
 
 const MainPage = () => {
+  const [thing, setThing] = useState(undefined);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/76341?api_key=${process.env.NEXT_PUBLIC_API_KEY}`
+      )
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  }, []);
   return (
     <CustomGridWrapper>
       <SearchBar />
