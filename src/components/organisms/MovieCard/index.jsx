1905af937e2df5d1a90bfc98385bc26813127da0
Greg Martin <gregm31@live.com>
2021-03-11 17:53:02 +1100

    WIP

--- src/components/organisms/MovieCard/index.jsx
+++ src/components/organisms/MovieCard/index.jsx
@@ -1,9 +1,34 @@
 import React from "react";
+import styled from "styled-components";
 
-const MovieCard = () => (
-  <>
-    <h1>Hello</h1>
-  </>
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+  grid-column: span 3;
+  background-image: props => props.image;
+  background-color: red;
+  border-radius: 12px;
+  height: 350px;
+  justify-content: flex-end;
+`;
+
+const Header = styled.h3``;
+
+const Description = styled.p`
+  color: #fff;
+  font-weight: 100;
+`;
+
+const MovieCard = ({ image, title, description }) => (
+  <Container img={image}>
+    <Header>{title}HEADER</Header>
+    <Description>
+      Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet. Lorem ipsum, dolor
+      sit amet consectetur adipisicing elit. Tenetur, repudiandae.
+      {description}
+    </Description>
+  </Container>
 );
 
 export default MovieCard;
